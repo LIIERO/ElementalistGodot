0347ba2a1b8a910f50bf327c22dc7d0a292d47af
Liero <lierotoma@gmail.com>
2025-06-07 00:17:59 +0200

    tiny fixes

--- Scripts/Entities/Interactable/Watchtower.cs
+++ Scripts/Entities/Interactable/Watchtower.cs
@@ -6,7 +6,7 @@ using static System.Collections.Specialized.BitVector32;
 public partial class Watchtower : Interactable
 {
     private GameState gameState; // Singleton
-    private bool unlockNextFrame = false;
+    //private bool unlockNextFrame = false;
 
     public override void _Ready()
 	{
@@ -18,28 +18,30 @@ public partial class Watchtower : Interactable
     public override void _Process(double delta)
     {
 
-        if (unlockNextFrame)
+        /*if (unlockNextFrame)
         {
             UnlockWatchtowerEndFrame();
             return;
-        }  
+        }  */
 
         if (!gameState.WatchtowerActive) return;
 
-        if (InputManager.JumpPressed() || InputManager.AbilityPressed() || InputManager.UpInteractPressed())
+        if (InputManager.JumpPressed() || InputManager.AbilityPressed())
         {
             gameState.WatchtowerActive = false;
-            unlockNextFrame = true;
+            //unlockNextFrame = true;
         }
     }
 
     protected override void Interact()
 	{
-        if (unlockNextFrame) return;
+        /*if (unlockNextFrame) return;
 
-        unlockNextFrame = true;
+        unlockNextFrame = true;*/
 
 		base.Interact();
+
+        if (gameState.WatchtowerActive) return;
 
         gameState.WatchtowerActive = true;
     }
@@ -51,9 +53,9 @@ public partial class Watchtower : Interactable
         gameState.WatchtowerActive = false;
     }
 
-    async void UnlockWatchtowerEndFrame()
+    /*async void UnlockWatchtowerEndFrame()
     {
         await ToSignal(GetTree(), "process_frame");
         unlockNextFrame = false;
-    }
+    }*/
 }
